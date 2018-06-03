0654b28aa68cbde640027a0c4ae513fd9460e04b
Sergey Savtyra <pandinator@protonmail.com>
2018-06-03 18:38:45 +0300

    Добавил данные на Signin

--- src/pages/Signin.vue
+++ src/pages/Signin.vue
@@ -3,25 +3,25 @@
     <div class="wrapper">
       <h1 class="logo-text">Ranty</h1>
       <p class="sub-title">Добро пожаловать! Войдите или зарегистрируйтесь</p>
-      <form class="form">
+      <form class="form" @submit="e => e.preventDefault()">
         <div class="form-group floating-label">
-          <input type="text" class="line-based" required />
+          <input type="email" :class="'line-based ' + (email.length > 0 ? 'static' : '')" v-model="email" required />
           <label>Email</label>
         </div>
         <div class="form-group floating-label">
-          <input type="password" class="line-based" required />
+          <input type="password" class="line-based" v-model="password" required />
           <label>Пароль</label>
         </div>
         <div class="half-splitted">
           <div class="form-group checkbox">
-            <input type="checkbox" id="remember-me" />
+            <input type="checkbox" id="remember-me" v-model="remember" />
             <label for="remember-me">Запомнить меня</label>
           </div>
           <router-link to="/recovery">Забыл пароль</router-link>
         </div>
         <div class="half-splitted">
           <button class="square-like yellow">Войти</button>
-          <button class="square-like">Регистрация</button>
+          <router-link to="/signup" class="square-like">Регистрация</router-link>
         </div>
       </form>
     </div>
@@ -32,5 +32,17 @@
 
 export default {
   name: 'signin',
+  data() {
+    return {
+      email: '',
+      password: '',
+      remember: false
+    }
+  },
+  methods: {
+    navigateToSignup() {
+      this.$router.push('/signup')
+    }
+  }
 }
 </script>
