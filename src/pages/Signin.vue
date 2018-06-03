3cf8b2d1e9a450f7ae27b56f2a5bab894d79e205
Sergey Savtyra <pandinator@protonmail.com>
2018-06-03 16:11:56 +0300

    Отрефакторил вёрстку в Signin

--- src/pages/Signin.vue
+++ src/pages/Signin.vue
@@ -1,28 +1,30 @@
 <template>
   <div class="authorization">
-    <h1 class="logo-title">Ranty</h1>
-    <p class="sub-title">Добро пожаловать! Войдите или зарегистрируйтесь.</p>
-    <form class="form">
-      <div class="form-group floating-label">
-        <input type="text" class="line-based" required />
-        <label>Email или телефон</label>
-      </div>
-      <div class="form-group floating-label">
-        <input type="password" class="line-based" required />
-        <label>Пароль</label>
-      </div>
-      <div class="half-splitted">
-        <div class="form-group checkbox">
-          <input type="checkbox" id="remember-me" />
-          <label for="remember-me">Запомнить меня</label>
+    <div class="wrapper">
+      <h1 class="logo-title">Ranty</h1>
+      <p class="sub-title">Добро пожаловать! Войдите или зарегистрируйтесь.</p>
+      <form class="form">
+        <div class="form-group floating-label">
+          <input type="text" class="line-based" required />
+          <label>Email или телефон</label>
         </div>
-        <router-link to="/recovery">Забыл пароль</router-link>
-      </div>
-      <div class="half-splitted">
-        <button class="square-like yellow">Войти</button>
-        <button class="square-like">Регистрация</button>
-      </div>
-    </form>
+        <div class="form-group floating-label">
+          <input type="password" class="line-based" required />
+          <label>Пароль</label>
+        </div>
+        <div class="half-splitted">
+          <div class="form-group checkbox">
+            <input type="checkbox" id="remember-me" />
+            <label for="remember-me">Запомнить меня</label>
+          </div>
+          <router-link to="/recovery">Забыл пароль</router-link>
+        </div>
+        <div class="half-splitted">
+          <button class="square-like yellow">Войти</button>
+          <button class="square-like">Регистрация</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -53,14 +55,10 @@ export default {
     text-align: center
     margin: 15px 0 30px 0
   .form
-    display: flex
-    align-items: center
-    flex-direction: column
-    margin-top: 15px
-    input[type="text"], input[type="password"]
-      width: 360px
+    width: 95%
+    margin: 15px auto 0 auto
     button
-      width: 170px
+      width: 47%
 
 .form-group
   position: relative
