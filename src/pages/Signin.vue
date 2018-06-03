bd906006b60a709ea53624a3fd3c5911a3bfe040
Sergey Savtyra <pandinator@protonmail.com>
2018-06-03 18:05:10 +0300

    Убрал точку в надписи после лого

--- src/pages/Signin.vue
+++ src/pages/Signin.vue
@@ -2,7 +2,7 @@
   <div class="authorization">
     <div class="wrapper">
       <h1 class="logo-text">Ranty</h1>
-      <p class="sub-title">Добро пожаловать! Войдите или зарегистрируйтесь.</p>
+      <p class="sub-title">Добро пожаловать! Войдите или зарегистрируйтесь</p>
       <form class="form">
         <div class="form-group floating-label">
           <input type="text" class="line-based" required />
@@ -34,46 +34,3 @@ export default {
   name: 'signin',
 }
 </script>
-
-<style lang="sass">
-.authorization
-  display: flex
-  justify-content: center
-  align-items: center
-  flex-direction: column
-  min-height: 100vh
-  .sub-title
-    color: #ADAEB4
-    text-align: center
-    margin: 15px 0 30px 0
-  .form
-    button
-      width: 47%
-
-@media (max-width: 500px)
-  .authorization
-    .half-splitted:last-of-type
-      flex-direction: column
-      justify-content: flex-start
-      align-items: center
-      button:last-of-type
-        margin-top: 15px
-    .form
-      *
-        font-size: 14px
-      button
-        &.yellow
-          width: 80%
-        &:not(.yellow)
-          border: none
-
-@media (max-width: 450px)
-  .authorization .form
-    padding: 0 15px
-
-
-@media (min-height: 1000px)
-  .authorization
-    .wrapper
-      transform: scale(1.6)
-</style>
