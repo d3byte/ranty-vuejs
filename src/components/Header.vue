9379e4e75c616c75de7db4d724373603f5e7d5da
Sergey Savtyra <pandinator@protonmail.com>
2018-06-16 02:14:56 +0300

    Начал делать компонент для верхнего бара с хлебными крошками

---
+++ src/components/Header.vue
@@ -0,0 +1,72 @@
+<template>
+<header>
+    <div class="breadcrumbs">
+        <div class="breadcrumb">Помещение
+            <i class="material-icons">keyboard_arrow_right</i>
+        </div>
+        <div class="breadcrumb">
+            Управление
+            <i class="material-icons">keyboard_arrow_right</i>
+        </div>
+    </div>
+    <nav>
+        <i class="material-icons">person</i>
+        <i class="material-icons">email</i>
+    </nav>
+</header>
+</template>
+
+<script>
+export default {
+    name: 'ranty-header',
+}
+</script>
+
+<style lang="sass" scoped>
+header
+    display: flex
+    align-items: center
+    justify-content: space-between
+    background: #707070
+    *
+        color: white
+
+.breadcrumbs
+    display: flex
+    align-items: center
+    padding: 5px 20px
+    margin-left: 40px
+    heigh
+    .breadcrumb
+        position: relative
+        display: flex
+        align-items: center
+        border: 1px solid transparent
+        &:hover
+            cursor: pointer
+            &:not(:last-child):after
+                width: calc(100% - 6px)
+            &:last-child:after
+                width: calc(100% - 24px)
+        &:last-of-type
+            i
+                opacity: 0
+        &:after
+            position: absolute
+            content: ''
+            width: 0
+            height: 2px
+            border-radius: 8px
+            bottom: -6px
+            background: white
+            transition: 0.3s
+
+nav
+    justify-self: right
+    padding: 0 60px 0 20px
+    &:hover
+        cursor: pointer
+    i
+        &:first-of-type
+            margin-right: 20px
+</style>
