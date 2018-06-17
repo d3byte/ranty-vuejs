970357217c7e88be49a0ed938302b47bb14276c3
Sergey Savtyra <pandinator@protonmail.com>
2018-06-18 00:26:33 +0300

    Баг фикс в компоненте дропдауна

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,5 +1,5 @@
 <template>
-<div :class="'form-group dropdown ' + classes">
+<div :class="'form-group dropdown ' + (classes || '')">
     <div class="name">
         <input type="checkbox" v-model="toggled" />
         <label>{{ (Object.keys(chosenItem).length > 0 ? chosenItem.name : defaultLabel) }}</label>
@@ -28,53 +28,3 @@ export default {
     },
 }
 </script>
-
-<style lang="sass">
-.form-group.dropdown
-    margin-bottom: 20px
-    input[type="checkbox"]
-        opacity: 0
-        position: absolute
-        display: block
-        top: 0
-        width: 100%
-        height: 100%
-        cursor: pointer
-        &:checked ~ img
-            transform: rotate(180deg)
-    .name
-        position: relative
-        background: transparent
-        border: none
-        width: 100%
-        display: flex
-        align-items: center
-        justify-content: space-between
-        border-bottom: 1px solid #E9E9EF
-        padding: 10px 20px
-        img
-            width: 24px 
-            transition: 0.3s
-            &:active
-                & ~ .items
-                    display: block
-    .items
-        overflow: hidden
-        border: 1px solid #E9E9EF
-        border-top: none
-        border-bottom-left-radius: 8px
-        border-bottom-right-radius: 8px
-        margin: 0
-        padding: 10px 20px
-        list-style: none
-        &:empty
-            padding: 0
-            border: none
-        li
-            margin-bottom: 15px
-            &:last-of-type
-                margin-bottom: 0
-            &:hover
-                cursor: pointer
-
-</style>
