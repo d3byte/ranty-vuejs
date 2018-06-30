3901175498ac3c5140930729b57c294b1b3912ff
Sergey Savtyra <pandinator@protonmail.com>
2018-06-30 23:44:17 +0300

    Vuex store

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,1 +1,5 @@
-export default {}
+export default {
+    login({ commit }, credentials) {
+        commit('login', credentials)
+    },
+}
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,1 +1,6 @@
-export default {}
+export default {
+    login(state, { user, token }) {
+        state.user = user
+        state.token = token
+    },
+}
