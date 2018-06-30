39164b1e48c7cdbc1351eb6a36347b4cf1bdc442
Sergey Savtyra <pandinator@protonmail.com>
2018-06-30 23:43:21 +0300

    конфиг vue-resource

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+Vue.use(VueResource)
+
 Vue.config.productionTip = false
+Vue.http.options.root = 'http://46.229.212.225/api'
+Vue.http.options.emulateJSON = true
+Vue.http.headers.common['application/json']
+
 
 new Vue({
   router,
