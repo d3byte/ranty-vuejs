ec79cccdf26919d0a67e8b5b737f1ec4190a37ee
Sergey Savtyra <pandinator@protonmail.com>
2018-07-01 15:13:58 +0300

    Теперь Vuex состояние сохраняется при обновлении страницы

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 import state from './state'
 import actions from './actions'
@@ -11,4 +12,5 @@ export default new Vuex.Store({
   state,
   mutations,
   actions,
+  plugins: [createPersistedState()],
 })
