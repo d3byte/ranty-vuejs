e6ff0f6a9a8a6811f3ee8670f43f64fe386deac6
Sergey Savtyra <pandinator@protonmail.com>
2018-07-18 22:28:48 +0300

    Адаптировал компонент Dropdown

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -2,11 +2,11 @@
 <div :class="'form-group dropdown ' + (classes || '')">
     <div class="name">
         <input type="checkbox" v-model="toggled" />
-        <label>{{ (Object.keys(chosenItem).length > 0 ? chosenItem.name : defaultLabel) }}</label>
+        <label>{{ (Object.keys(chosenItem).length > 0 ? chosenItem.name || chosenItem.title : defaultLabel) }}</label>
         <img src="@/assets/arrow_down.svg" @error="this.src = '@/assets/arrow_down.png'"/>
     </div>
     <ul class="items" v-show="toggled">
-        <li v-for="item in items" :key="item.id" @click="() => selectItem(item)">{{ item.name }}</li>
+        <li v-for="item in items" :key="item.id" @click="() => selectItem(item)">{{ item.name || item.title }}</li>
     </ul>
 </div>  
 </template>
